2878dd8b3f3c672b8dcdaf85bf8fdb28116c553d
Ultimatinium <79572476+Ultimatinium@users.noreply.github.com>
2022-08-18 14:50:57 +0800

    Update download.sh

--- tiktok/download.sh
+++ tiktok/download.sh
@@ -1,68 +1,13 @@
-#!/bin/bash
+import requests
+import re
+import subprocess
 
-echo "Declaring variables"
-declare -A apks
+TIKTOK_URL = "https://tiktok.en.uptodown.com/android/download"
 
-apks["com.zhiliaoapp.musically.apk"]=dl_tiktok
+def download_apk(url, filename):
+    response = requests.get(url)
+    dl_url = re.findall(r'(?<=href=")https:\/\/dw.uptodown.com.*?(?=")', response.content.decode("utf-8"))[0]
+    subprocess.run(["wget", dl_url, "-O", filename])
 
-WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
-
-req()
-{ wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
-
-get_apk_vers()
-{ req "$1" - | sed -n 's;.*Version:</span><span class="infoSlide-value">\(.*\) </span>.*;\1;p'; }
-
-get_largest_ver()
-{
-    local max=0
-    while read -r v || [ -n "$v" ]
-    do
-        if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]
-	then max=$v
-	fi
-    done
-    if [[ $max = 0 ]]
-    then echo ""
-    else echo "$max"
-    fi
-}
-
-dl_apk()
-{
-    local url=$1 regexp=$2 output=$3
-    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
-    echo "$url"
-    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-    req "$url" "$output"
-}
-
-dl_tiktok()
-{
-    echo "Downloading TikTok"
-    local last_ver
-    last_ver="$version"
-    last_ver="${last_ver:-$(get_apk_vers "https://www.apkmirror.com/uploads/?appcategory=tik-tok-including-musical-ly" | get_largest_ver)}"
-
-    echo "Selected version: ${last_ver}"
-    local base_apk="com.zhiliaoapp.musically.apk"
-    if [ ! -f "$base_apk" ]
-    then
-        declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/tiktok-pte-ltd/tik-tok-including-musical-ly/tik-tok-including-musical-ly-${last_ver//./-}-release/" \
-                "APK</span>[^@]*@\([^#]*\)" \
-                "$base_apk")
-        echo "TikTok v${last_ver}"
-        echo "Downloaded from: [TikTok - APKMirror]($dl_url)"
-    fi
-}
-
-for apk in "${!apks[@]}"
-do
-    if [ ! -f $apk ]
-    then
-        echo "Downloading $apk"
-        version=$(jq -r ".\"$apk\"" <versions.json)
-        ${apks[$apk]}
-    fi
-done
+print("Downloading Tiktok")
+download_apk(TIKTOK_URL, "com.zhiliaoapp.musically")
