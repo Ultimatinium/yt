ac98a5078b8b6cf6f4550a14c6765a73955214ee
Ultimatinium <79572476+Ultimatinium@users.noreply.github.com>
2022-08-19 13:55:35 +0800

    Update compile.sh

--- yt-music-v7a/compile.sh
+++ yt-music-v7a/compile.sh
@@ -37,6 +37,10 @@ if [[ "$1" == "clean" ]]; then
     exit
 fi
 
+if [[ "$1" == "experimental" ]]; then
+    EXPERIMENTAL="--experimental"
+fi
+
 echo "Fetching dependencies"
 for artifact in "${!artifacts[@]}"
 do
@@ -59,8 +63,9 @@ if [ -f "com.google.android.apps.youtube.music.apk" ]
 then
     echo "Compiling package"
     java -jar revanced-cli.jar -b revanced-patches.jar \
-                               ${patches[@]} \
-                               -a com.google.android.apps.youtube.music.apk -o output/yt-music-v7a.apk
+        ${patches[@]} \
+        $EXPERIMENTAL \
+        -a com.google.android.apps.youtube.music.apk -o build/yt-music-v7a.apk
 else
     echo "Cannot find YouTube Music arm-v7a base package, skip compiling"
 fi
