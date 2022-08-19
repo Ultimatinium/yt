ed491330d9d20e63e67d6ae86fe932ec2013793f
Ultimatinium <79572476+Ultimatinium@users.noreply.github.com>
2022-08-19 13:28:19 +0800

    Update download.py

--- yt-music-v7a/download.py
+++ yt-music-v7a/download.py
@@ -1,13 +1,78 @@
-import requests
-import re
-import subprocess
+#!/bin/bash
 
-YT_MUSIC_URL = "https://youtube-music.en.uptodown.com/android/download"
+echo "Declaring variables"
+declare -A apks
 
-def download_apk(url, filename):
-    response = requests.get(url)
-    dl_url = re.findall(r'(?<=href=")https:\/\/dw.uptodown.com.*?(?=")', response.content.decode("utf-8"))[0]
-    subprocess.run(["wget", dl_url, "-O", filename])
+apks["com.google.android.apps.youtube.music.apk"]=dl_youtube-music
 
-print("Downloading YouTube Music arm-v7a")
-download_apk(YT_MUSIC_URL, "com.google.android.apps.youtube.music.apk")
+WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+ARM_V7A="arm-v7a"
+ARM64_V8A="arm64-v8a"
+
+req()
+{ wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
+
+get_apk_vers()
+{ req "$1" - | sed -n 's;.*Version:</span><span class="infoSlide-value">\(.*\) </span>.*;\1;p'; }
+
+get_largest_ver()
+{
+    local max=0
+    while read -r v || [ -n "$v" ]
+    do
+        if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]
+	then max=$v
+	fi
+    done
+    if [[ $max = 0 ]]
+    then echo ""
+    else echo "$max"
+    fi
+}
+
+dl_apk()
+{
+    local url=$1 regexp=$2 output=$3
+    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
+    echo "$url"
+    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+    url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+    req "$url" "$output"
+}
+
+dl_youtube-music()
+{
+    local arch=$ARM_V7A
+    echo "Downloading YouTube Music (${arch})"
+    local last_ver
+    last_ver="$version"
+    last_ver="${last_ver:-$(get_apk_vers "https://www.apkmirror.com/uploads/?appcategory=youtube-music" | get_largest_ver)}"
+
+    echo "Selected version: ${last_ver}"
+    local base_apk="com.google.android.apps.youtube.music.apk"
+    if [ ! -f "$base_apk" ]
+    then
+        if [ "$arch" = "$ARM_V7A" ]
+	then
+            local regexp_arch='armeabi-v7a</div>[^@]*@\([^"]*\)'
+	elif [ "$arch" = "$ARM64_V8A" ]
+	then
+            local regexp_arch='arm64-v8a</div>[^@]*@\([^"]*\)'
+        fi
+        declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music-${last_ver//./-}-release/" \
+                "$regexp_arch" \
+                "$base_apk")
+        echo "YouTube Music (${arch}) v${last_ver}"
+        echo "Downloaded from: [YouTube Music (${arch}) - APKMirror]($dl_url)"
+    fi
+}
+
+for apk in "${!apks[@]}"
+do
+    if [ ! -f $apk ]
+    then
+        echo "Downloading $apk"
+        version=$(jq -r ".\"$apk\"" <v7a.json)
+        ${apks[$apk]}
+    fi
+done
